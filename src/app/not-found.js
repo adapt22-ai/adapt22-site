9c01f80b9e9ee2ea600e7034f2a9dd25571d94fe
Jackson McConnell <jackson@adapt22.ai>
2025-04-12 14:43:03 +0100

    Add working not-found.js to fix Vercel build

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+// src/app/not-found.js
+
+export default function NotFound() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-[#f3f5f3] text-gray-800 px-6">
+        <div className="text-center space-y-6">
+          <h1 className="text-6xl font-extrabold text-primary">404</h1>
+          <p className="text-xl">Page not found.</p>
+        </div>
+      </div>
+    );
+  }
+  
