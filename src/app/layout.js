e9e78654b4690c1cfd7bb994ae57ec8fe98e05a7
Jackson McConnell <jackson@adapt22.ai>
2025-05-09 17:57:31 +0100

    flav fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,8 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
         <Script src="https://assets.calendly.com/assets/external/widget.js" strategy="lazyOnload" />
         <Script id="calendly-inline-widget" strategy="lazyOnload">
