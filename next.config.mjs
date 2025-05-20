b6d9e3d6fdc32ae5900eba84b93fef82f01ffca4
Jackson McConnell <jackson@adapt22.ai>
2025-05-20 18:32:24 +0100

    newish update x

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/faq',
+        destination: '/faqs',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
